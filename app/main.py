ec49bec957de75329b9f61b488b73371327370ac
Elias Ortiz <elortiz@uaa.edu.py>
2025-11-10 22:01:00 -0300

    Documentado de main

--- app/main.py
+++ app/main.py
@@ -1,18 +1,21 @@
-from fastapi import FastAPI
-from app.routes import users, tasks
-from app.database import Base, engine
 
-# Crear las tablas  en la base de datos 
+# Impostamos FastAPI  para crear la aplicacion 
+from fastapi import FastAPI
+from app.routes import users, tasks  # importamos las rutas de usuarios  y tareas 
+from app.database import Base, engine # Importamos la base  y el motor de base de datos 
+
+# Crear las tablas  en la base de datos ( si no extsten)
 Base.metadata.create_all(bind-engine)
 
+# Creamos la instancia principal de  FastAPI
 app = FastAPI(title = "Backend test API")
 
 
-# Rutas 
+# Incluimos las  rutas de usuarios y tareas 
 app.include_route(users.router)
 app.include_route(tasks.router)
 
-
+# Ruta principal (inicio)
 @app.get("/")
 def root():
-    return{"message":"API de prueba técnica "}
+    return{"message":"API de prueba técnica "} # devuelve un mensaje simple 
