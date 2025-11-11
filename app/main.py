59da6f224978653a82b912aad5a6c4d27c16f076
Elias Ortiz <elortiz@uaa.edu.py>
2025-11-10 21:25:33 -0300

    inicio  desarrollo API

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.routes import users, tasks
+from app.database import Base, engine
+
+# Crear las tablas  en la base de datos 
+Base.metadata.create_all(bind-engine)
+
+app = FastAPI(title = "Backend test API")
+
+
+# Rutas 
+app.include_route(users.router)
+app.include_route(tasks.router)
