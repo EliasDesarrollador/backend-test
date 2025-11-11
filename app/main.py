3da3812a243ce34f881bcfb70093f67a95a44e60
Elias Ortiz <elortiz@uaa.edu.py>
2025-11-10 21:31:20 -0300

    Function root

--- app/main.py
+++ app/main.py
@@ -11,3 +11,8 @@ app = FastAPI(title = "Backend test API")
 # Rutas 
 app.include_route(users.router)
 app.include_route(tasks.router)
+
+
+@app.get("/")
+def root():
+    return{"message":"API de prueba técnica "}
