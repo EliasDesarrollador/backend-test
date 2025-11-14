60fafe49934fff6eb6e462767e8ce865ed805c59
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-11-13 22:56:12 -0300

    trabajo en database.py

--- app/database.py
+++ app/database.py
@@ -9,5 +9,20 @@ SQALCHEMY_DATABASE_URL ="sqlite:///./test.db"
 # Creamos e motor  de conexion (engine)
 #  "connect_args" solo  se usa  con SQLite para permitir  acceso desde distintos  hilos
 engine = create_engine(
-    SQALCHEMY_DATABASE_URL, connect_args=("check_same_thread": False)
+    SQALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+#Crea una clase para manejar las sesiones (conexiones) a la base 
+SessionLocal = sessionmaker(autpcommit= False, autoflush=False, bind=engine)
+
+# Clase base para los modelos  ( tablas)
+Base = declarative_base()
+
+#Dependencia que permite obtener las sesion  de base de datos 
+#(se usa dentro  de los endpoints)
+def get_db():
+         db = SessionLocal() # Crea  una sesion nueva
+         try:
+              yield db  # la devuelve para usarla 
+         finally:
+                db.close() # Cierra la conexion al terminar 
