66a48ebc260faef9a53866da53a233fcb6b2b0a2
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-11-17 22:55:25 -0300

    importaciones de herramientas necesarias de SQLAlchemy

---
+++ app/models.py
@@ -0,0 +1,6 @@
+
+# Importamos  las herramientas necesarias de SQLAlchemy
+from sqlalchemy import Column, Integer ,String, ForeighKey
+from sqlalchemy.orm import relarionship
+from app.database import Base
+
