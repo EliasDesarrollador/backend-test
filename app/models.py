dcef2ab6092c4b3f67552b53a3ece21e9c3488c0
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-11-17 22:56:28 -0300

    modelo para la tabla de usuarios

--- app/models.py
+++ app/models.py
@@ -4,3 +4,6 @@ from sqlalchemy import Column, Integer ,String, ForeighKey
 from sqlalchemy.orm import relarionship
 from app.database import Base
 
+#Modelo  para la tabla de usuarios 
+class User(Base):
+    __tablename__ = "users" # Nombre de  la tabla en la base
