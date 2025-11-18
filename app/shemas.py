7577733b82029de2f24ed0d030d58998b2602c5d
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-11-17 23:33:20 -0300

    esquema base

--- app/shemas.py
+++ app/shemas.py
@@ -3,3 +3,11 @@
 from pydantic import BaseModel, EmailStr
 from typing  import Optional, List 
 
+# ------------SCHEMAS DE TAREAS ---------------
+
+# Esquema base:  define los campos comunes de una tarea 
+class TaskBase(BaseModel):
+    titulo: str                                                 # Campo obligatorio
+    descripcion: Opctional[str] = None  # Campo opcional
+    estado: Optional[str] = "pendiente" # Valor por defecto
+    
