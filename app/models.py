06c2b69093234362dcedb6a6a0743e30efc5c8a0
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-11-17 23:09:38 -0300

    columnas de la tabla

--- app/models.py
+++ app/models.py
@@ -7,3 +7,10 @@ from app.database import Base
 #Modelo  para la tabla de usuarios 
 class User(Base):
     __tablename__ = "users" # Nombre de  la tabla en la base
+
+    # Columnas de la tabla  
+    id = Column(Integer, primary_key=True, index=True) # ID autoincremental 
+    titulo = Column(String, nullable=False) # Titulo de la tarea
+    descripcion = Column(String ) #Descripcion
+    estado = Column(String, default ="pendiente") # Estado por  defecto
+    id_usuario = Column(Integer, ForeignKey("users.id")) # Clave foranea(usuario dueno) 
