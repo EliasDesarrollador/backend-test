119264e53944926a8356a71f7eb48ad099286c25
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-11-20 22:33:32 -0300

    router para tareas

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -5,3 +5,16 @@ from sqlalchemy.orm  import Session
 from app.schemas import TaskCreate, Task 
 from app.models import Task as TaskModel, User  as UserModel 
 from app.database import get_db
+
+
+#Router para  tareas 
+router = APIRouter(
+    prefix="/tasks",    # Todas las rutas empiezan con /tasks 
+    tags=["Tareas"]
+)
+
+
+
+#   -----------------------------------------------------------
+#                        CREAR UNA TAREA 
+# ------------------------------------------------------------
